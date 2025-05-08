1f5ba7743605f3c36fc63fae70b4997799f3f096
Tonypadgett <157521742+Tonypadgett@users.noreply.github.com>
2025-05-08 14:28:18 -0400

    Create Global.cs

---
+++ Global.cs
@@ -0,0 +1,12 @@
+using static IIDKQuest.Menu.Main;
+
+namespace IIDKQuest.Mods
+{
+    internal class Global
+    {
+        public static void ReturnHome()
+        {
+            buttonsType = 0;
+        }
+    }
+}
